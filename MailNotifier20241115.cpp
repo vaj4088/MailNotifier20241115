1ae8dc7e288a9579596bcc87186ae5974ec8bc87
vaj4088 <vaj4088@CoyoteLinux20241102>
2024-11-15 18:43:43 -0600

    Gettng ready for snapshot and new start on MailNotifier.

---
+++ MailNotifier20241115.cpp
@@ -0,0 +1,15 @@
+// Do not remove the include below
+#include "MailNotifier20241115.h"
+
+
+//The setup function is called once at startup of the sketch
+void setup()
+{
+// Add your initialization code here
+}
+
+// The loop function is called in an endless loop
+void loop()
+{
+//Add your repeated code here
+}
---
+++ MailNotifier20241115.h
@@ -0,0 +1,22 @@
+// Only modify this file to include
+// - function definitions (prototypes)
+// - include files
+// - extern variable definitions
+// In the appropriate section
+
+#ifndef _MailNotifier20241115_H_
+#define _MailNotifier20241115_H_
+#include "Arduino.h"
+//add your includes for the project MailNotifier20241115 here
+
+
+//end of add your includes here
+
+
+//add your function definitions for the project MailNotifier20241115 here
+
+
+
+
+//Do not add code below this line
+#endif /* _MailNotifier20241115_H_ */
